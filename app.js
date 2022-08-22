557de8add6a40d5119e48f1b3c4ce71b5857a55c
Denis Obolevich <denzelwash33@mail.ru>
2022-08-22 18:09:27 +0300

    added console messages

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import getArgs from './helpers/args.js'
+import { printError, printSuccess, printHelp } from './services/log.service.js'
 
 const initCli = () => {
   const args = getArgs(process.argv)
   console.log(args)
+  printError()
+  printSuccess()
+  printHelp()
 }
 
 initCli()
--- services/log.service.js
+++ services/log.service.js
@@ -1,3 +1,23 @@
 import chalk from 'chalk'
+import dedent from 'dedent'
 
-console.log(chalk.red('Hello world!'))
+const printSuccess = (message) => {
+  console.log(chalk.bgGreen('SUCCESS') + ' ' + message)
+}
+
+const printError = (err) => {
+  console.log(chalk.bgRed('ERROR') + ' ' + err)
+}
+
+const printHelp = () => {
+  console.log(
+    dedent`${chalk.bgCyan(' HELP ')}
+          Без параметров - вывод погоды
+          -s [CITY] для установки города
+          -h для вывода помощи
+          -t [API_KEY] для сохранения токена
+    `
+  )
+}
+
+export { printError, printSuccess, printHelp }
