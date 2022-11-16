2c2fe393caab7b65c0414ae820cde76e2b2cd45c
Denis Obolevich <denzelwash33@mail.ru>
2022-11-16 20:13:15 +0300

    добавил вывод лога

--- weather.js
+++ weather.js
@@ -5,10 +5,12 @@ import { printError, printSuccess, printHelp } from './services/log.service.js'
 
 const initCli = () => {
   const args = getArgs(process.argv)
-  console.log(args)
   if (args.h) {
+    printHelp()
   }
   if (args.s) {
+    saveKeyValue('city', args.s)
+    printSuccess('Город сохранен')
   }
   if (args.t) {
     saveToken(args.t)
@@ -17,6 +19,9 @@ const initCli = () => {
 }
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан токен')
+  }
   try {
     await saveKeyValue('token', token)
     printSuccess('Токен сохранен')
